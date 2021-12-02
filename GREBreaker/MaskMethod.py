cb05303cf1e402bc49a880a3b756d7c7ad2d77ce
Andy Chen <chen.andy14@gmail.com>
2021-12-02 11:12:00 -0500

    Able to calculate closeness of words predicted by BERT with question choices.

--- GREBreaker/MaskMethod.py
+++ GREBreaker/MaskMethod.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from ast import literal_eval
 
 import transformers
 from transformers import BertModel, BertTokenizer, BertForMaskedLM, AdamW, get_linear_schedule_with_warmup
@@ -60,6 +61,8 @@ print(encoding['attention_mask'])
 
 print(tokenizer.convert_ids_to_tokens(encoding['input_ids'][0]))
 
+
+# creating train, eval, test datasets
 df_train, df_test = train_test_split(df, test_size=0.5, random_state=RANDOM_SEED)
 df_train, df_val = train_test_split(df_train, test_size=0.1, random_state=RANDOM_SEED)
 print("Train size: ", df_train.shape)
@@ -67,6 +70,7 @@ print("Val size: ", df_val.shape)
 print("Test size: ", df_test.shape)
 
 
+# helper function for predicting masked word
 def predict_masked_sent(text, top_k=5):
     # Tokenize input
     text = "[CLS] %s [SEP]"%text
@@ -84,18 +88,102 @@ def predict_masked_sent(text, top_k=5):
     probs = torch.nn.functional.softmax(predictions[0, masked_index], dim=-1)
     top_k_weights, top_k_indices = torch.topk(probs, top_k, sorted=True)
 
+    predicted_words = []
     for i, pred_idx in enumerate(top_k_indices):
         predicted_token = tokenizer.convert_ids_to_tokens([pred_idx])[0]
         token_weight = top_k_weights[i]
         print("[MASK]: '%s'"%predicted_token, " | weights:", float(token_weight))
+        predicted_words.append(predicted_token)
 
+    return predicted_words
+
+
+
+# importing GLoVe embedding
+filenameGlove = "glove.6B.50d"
+gloveEmbedding = { "<PAD>": np.zeros(200), "UNKA": np.random.rand(200) }
+gloveEmbeddingIdx = { "<PAD>": 0, "UNKA": 1 }
+nGloveEmbeddingDim = 0
+
+fileGlove = open("../glove_data/"+filenameGlove+".txt", "r")
+
+try:
+  print("Loading "+filenameGlove+" weights from cache.")
+  gloveEmbeddingMatrix = np.load("../glove_data/"+filenameGlove+"_weights.npy")
+  gloveEmbeddingMatrix = torch.tensor(gloveEmbeddingMatrix.astype(float))
+  for ii, line in enumerate(fileGlove):
+    try:
+      lineTokens = line.split(" ")
+      gloveEmbeddingIdx[lineTokens[0]] = len(gloveEmbeddingIdx)
+      if nGloveEmbeddingDim == 0:
+        nGloveEmbeddingDim = len(lineTokens) - 1
+    except:
+      print("Issue with line ", ii, ".")
+      print(lineTokens)
+except:
+  print("No cache found. Loading from txt and generating cache.")
+  for ii, line in enumerate(fileGlove):
+    try:
+      lineTokens = line.split(" ")
+      lineTokens[-1] = lineTokens[-1][:-1]  # removes newline character
+      gloveEmbedding[lineTokens[0]] = np.array(lineTokens[1:]).astype(float)
+      gloveEmbeddingIdx[lineTokens[0]] = len(gloveEmbeddingIdx)
+      if nGloveEmbeddingDim == 0:
+        nGloveEmbeddingDim = len(lineTokens) - 1
+    except:
+      print("Issue with line ", ii, ".")
+      print(lineTokens)
+  gloveEmbedding["<PAD>"] = np.zeros(nGloveEmbeddingDim)
+  gloveEmbedding["UNKA"] = np.random.rand(nGloveEmbeddingDim)
+  gloveEmbeddingMatrix = np.array(list(gloveEmbedding.values()))
+  np.save(filenameGlove+"_weights.npy", gloveEmbeddingMatrix)
+  gloveEmbeddingMatrix = torch.tensor(gloveEmbeddingMatrix.astype(float))
+
+nGloveVocab = len(gloveEmbeddingIdx)
+print("Size of embedding matrix: ", gloveEmbeddingMatrix.size())
+
+#model = RNNTagger(nGloveEmbeddingDim, hiddenSize, nTag)
+#opt = optim.Adam(model.parameters(), lr=0.001)
+#lossFunc = nn.NLLLoss()
+
+paddingIdx = gloveEmbeddingIdx["<PAD>"]
+wordEmbedding = nn.Embedding(num_embeddings=nGloveVocab, embedding_dim=nGloveEmbeddingDim, padding_idx=paddingIdx)
+wordEmbedding.load_state_dict({'weight': gloveEmbeddingMatrix})
+
+print()
+
+# running over train, eval, test datasets
 for ii in df_train.index:
   print(df_train["question"][ii])
+
+  # embeddings for choice words
+  choice_words = []
+  choice_words_idx = []
   for choice in choices:
-    print(df_train[choice][ii])
-  predict_masked_sent(df_train["question"][ii], top_k=5)
+    word = literal_eval(df_train[choice][ii])[0][0]
+    choice_words.append(word)
+    choice_words_idx.append(gloveEmbeddingIdx[word.lower()])
+
+  choice_words_idx = torch.tensor(np.array(choice_words_idx)).int()
+  print("Choice word indices: ", choice_words_idx)
+  choice_word_embeddings = wordEmbedding(choice_words_idx).view(-1, 1, nGloveEmbeddingDim)
+
+  # embeddings for predicted words
+  predicted_words = predict_masked_sent(df_train["question"][ii], top_k=5)
+  predicted_words_idx = []
+  for predicted_word in predicted_words:
+    predicted_words_idx.append(gloveEmbeddingIdx[predicted_word.lower()])
+
+  predicted_words_idx = torch.tensor(np.array(predicted_words_idx)).int()
+  print("Predicted word indices: ", predicted_words_idx)
+  predicted_word_embeddings = wordEmbedding(predicted_words_idx).view(-1, 1, nGloveEmbeddingDim)
+
   print()
   break
+
+print("Best prediction: ", predicted_words[0])
+for ii, embedding in enumerate(choice_word_embeddings):
+  print("Closeness of ", choice_words[ii], ": ", torch.dot(predicted_word_embeddings[0].view(-1), embedding.view(-1)) / (torch.norm(predicted_word_embeddings[0])*torch.norm(embedding)) )
 
 
 
