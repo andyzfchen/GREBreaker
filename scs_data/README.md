45ef5c8bd60a3b56349f10f0e39a40f119473ad8
Andy Chen <chen.andy14@gmail.com>
2021-12-05 11:29:18 -0500

    Added the SCS dataset with instruction and data preprocessing script.

---
+++ scs_data/README.md
@@ -0,0 +1,3 @@
+# SCS Dataset
+
+The full dataset for the 152 SCS questions can be found [here](https://drive.google.com/file/d/1Pc_Lhetn2s4QHOapjJHqsUCcGvTtZeq3/view?usp=sharing).
---
+++ scs_data/process_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import tokenize
+import re
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+
+df1 = pd.read_csv("../scs_data/scs_training.csv")
+print(df1.head())
+print(df1.columns)
+print(df1.shape)
+
+#print("Making dataset with "+str(n_blank)+" blanks.")
+
+n_row, n_col = df1.shape
+
+questions = []
+answers = []
+choice_a = []
+choice_b = []
+choice_c = []
+choice_d = []
+choice_e = []
+
+column_names = [ "a)", "b)", "c)", "d)", "e)" ]
+choice_names = [ "a", "b", "c", "d", "e" ]
+choice_names_temp = [ "a", "b", "c", "d", "e" ]
+choices = [ choice_a, choice_b, choice_c, choice_d, choice_e ]
+
+max_len = 0
+
+for jj in df1.index:
+  np.random.shuffle(choice_names_temp)
+
+  mask_idx = np.where([word1 != word2 for word1, word2 in zip(df1["a"][jj].split(), df1["b"][jj].split())])[0][0]
+
+  question = df1["a"][jj].split()
+  question[mask_idx] = re.sub("[A-Za-z]+", "[MASK]", question[mask_idx])
+  n_word = len(question)
+  question = " ".join(question)
+  questions.append(question)
+  
+
+  answer = choice_names[np.where([word == "a" for word in choice_names_temp])[0][0]]
+  answers.append(answer)
+  
+  for ii in range(len(choice_names)):
+    tokens = df1[choice_names_temp[ii]][jj].split()
+    assert len(tokens) == n_word
+    try:
+      choice = re.search("[A-Za-z]+", tokens[mask_idx])[0]
+    except:
+      for kk in range(len(choice_names)):
+        print(df1[choice_names_temp[kk]][jj])
+      print(tokens)
+      print(tokens[mask_idx])
+      exit()
+    choices[ii].append([[choice]])
+
+  if (jj+1)%500 == 0:
+    print("Processed ", jj+1, " rows of ", n_row, ".")
+
+data = {}
+data["ans"] = answers
+data["question"] = questions
+for ii in range(len(choice_names)):
+  data[column_names[ii]] = choices[ii]
+
+df_out = pd.DataFrame(data, columns=data.keys())
+df_out.to_csv("SCS_set_1blank.csv")
+      
